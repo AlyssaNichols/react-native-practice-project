0d0a95e409e7332693dc393b871293973adc034a
Alyssa Nichols <asnichols@scheels.com>
2023-12-26 11:56:34 -0600

    finished part 1 of mosh course, no navigation between pages yet

---
+++ app/components/Test.js
@@ -0,0 +1,8 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Test(props) {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({});
